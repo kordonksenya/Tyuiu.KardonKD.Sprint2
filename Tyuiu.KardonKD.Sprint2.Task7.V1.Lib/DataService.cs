2dd5b776d50abe5dbe66fb396c03cf94cd4acef6
Bilous.liza@bk.ru <Bilous.liza@bk.ru>
2025-04-05 22:36:51 +0500

    7

---
+++ Tyuiu.KardonKD.Sprint2.Task7.V1.Lib/DataService.cs
@@ -0,0 +1,21 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.KardonKD.Sprint2.Task7.V1.Lib
+{
+    public class DataService : ISprint2Task7V1
+    {
+        public bool CheckDotInShadedArea(double x, double y)
+        {
+            bool res;
+            if (((y >= x) && (y >= -x)) && (((x >= -Math.Sqrt(2) / 2) && (x <= Math.Sqrt(2) / 2)) && ((y >= 0) && (y <= 1))))
+            {
+                res = true;
+            }
+            else
+            {
+                res = false;
+            }
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.KardonKD.Sprint2.Task7.V1/Program.cs
@@ -0,0 +1,44 @@
+﻿using Tyuiu.KardonKD.Sprint2.Task7.V1.Lib;
+namespace Tyuiu.KardonKD.Sprint2.Task7.V1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #2 | Выполнила:Кордон К.Д. | ИСТНб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту                 *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #1                                                              *");
+            Console.WriteLine("* Выполнила: Кордон Ксения Дмитриевна | ИСТНб-24-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу на C#, которая запрашивает исходные данные и         *");
+            Console.WriteLine("* вычисляет, находится ли точка с координатами x,y в заштрихованной       *");
+            Console.WriteLine("* области                                                                 *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(" Введите значение переменной x: ");
+            double x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(" Введите значение переменной y: ");
+            double y = Convert.ToInt32(Console.ReadLine());
+            bool res = ds.CheckDotInShadedArea(x, y);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            if (res)
+            {
+                Console.WriteLine("Точка находится в заштрихованной области");
+            }
+            else
+            {
+                Console.WriteLine("Точка не находится в заштрихованной области");
+            }
+            Console.ReadKey();
+        }
+    }
+    
+}
